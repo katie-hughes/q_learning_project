bc4cb372fb3c0a37ac82feeb04e74204bf2c6467
Elizabet Singer <ensinger@uchicago.edu>
2021-02-22 00:32:04 -0600

    testing pub/sub

--- scripts/q_learning.py
+++ scripts/q_learning.py
@@ -19,14 +19,14 @@ class Q_Learning(object):
         rospy.init_node('turtlebot3_q_learning')
 
         # Create a publisher to the qmatrix 
-        self.q_matrix_pub = rospy.Publisher("/q_learning/QMatix", QMatrix, queue_size=10)
+        self.q_matrix_pub = rospy.Publisher("/q_learning/QMatix", QMatrix, queue_size=1)
         # self.q_matrix_pub(QMatrix) to publish the q-matrix
 
         # Create subscriber for reward of message type QLearningReward
         rospy.Subscriber("/q_learning/reward", QLearningReward, self.reward_received)
         
         # Create a publisher for robot actions
-        self.robot_action_pub = rospy.Publisher("/q_learning/robot_action", RobotMoveDBToBlock, queue_size=10)
+        self.robot_action_pub = rospy.Publisher("/q_learning/robot_action", RobotMoveDBToBlock, queue_size=1)
 
     def reward_received(data):
         reward = data.reward
@@ -36,11 +36,25 @@ class Q_Learning(object):
     def run(self):
         robot_action_data = RobotMoveDBToBlock()
         robot_action_data.robot_db = "red"
-        robot_action_data.block_id = "1"
+        robot_action_data.block_id = 1
         print("about to publish action")
         self.robot_action_pub.publish(robot_action_data)
         print("just published action")
-        
+
+        robot_action_data = RobotMoveDBToBlock()
+        robot_action_data.robot_db = "green"
+        robot_action_data.block_id = 2
+        print("about to publish action")
+        self.robot_action_pub.publish(robot_action_data)
+        print("just published action")
+
+        robot_action_data = RobotMoveDBToBlock()
+        robot_action_data.robot_db = "blue"
+        robot_action_data.block_id = 3
+        print("about to publish action")
+        self.robot_action_pub.publish(robot_action_data)
+        print("just published action")
+
         rospy.spin()
 
 
