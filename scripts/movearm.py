9eab8366bab54265b87c283cc5036025875d0883
Elizabet Singer <ensinger@uchicago.edu>
2021-02-28 01:48:15 -0600

    can now orient, appraoch, lift, move, and set down db properly

--- scripts/movearm.py
+++ scripts/movearm.py
@@ -24,7 +24,7 @@ class Robot(object):
         # openmanipulator gripper
         self.move_group_gripper = moveit_commander.MoveGroupCommander("gripper")
 
-                #Declare node as a subscriber to the scan topic and
+        #Declare node as a subscriber to the scan topic and
         # set self.process_scan as the function to be used for callback
         rospy.Subscriber("/scan", LaserScan, self.process_scan)
 
@@ -47,16 +47,16 @@ class Robot(object):
         
     def close_gripper(self):
         gripper_joint_goal = self.move_group_gripper.get_current_joint_values()
-        gripper_joint_goal = [0.0, 0.0]
+        gripper_joint_goal = [-0.005, -0.005]
         self.move_group_gripper.go(gripper_joint_goal, wait=True)
         self.move_group_gripper.stop()        
 
     def reach_dumbbell(self):
         arm_joint_goal = self.move_group_arm.get_current_joint_values()
         arm_joint_goal[0]=0.0
-        arm_joint_goal[1]=0.75
+        arm_joint_goal[1]=0.85
         arm_joint_goal[2]=-0.4
-        arm_joint_goal[3]=-0.35
+        arm_joint_goal[3]=-0.55
         self.move_group_arm.go(arm_joint_goal, wait=True)
         self.move_group_arm.stop()
 
@@ -65,51 +65,65 @@ class Robot(object):
         arm_joint_goal=[0.0,-.8,-0.2,0.3]
         self.move_group_arm.go(arm_joint_goal, wait=True)
         self.move_group_arm.stop()
+        rospy.sleep(1)
 
     def set_dumbbell(self):
         arm_joint_goal = self.move_group_arm.get_current_joint_values()
-        arm_joint_goal=[0.0,0.75,-0.75,0.0]
+        arm_joint_goal=[0.0,0.95,-0.75,-0.3]
         self.move_group_arm.go(arm_joint_goal, wait=True)
         self.move_group_arm.stop()
+        self.open_gripper()
+        self.twist.linear.x = -.05
+        self.twist_pub.publish(self.twist)
+        rospy.sleep(2)
+        self.twist.linear.x = 0
+        self.twist_pub.publish(self.twist)
 
     def face_dumbbell(self,data):
-        rotspeed = math.pi/4 # 180 degrees per second
+        rotspeed = math.pi/4 # 90 degrees per second
         range_data = np.array(data.ranges) #convert to numpy array
         minval = min(range_data) # closest point in space
-        mindir = np.argmin(range_data) # find direction to db
-        if (mindir == 0):
+        vals = np.argwhere(range_data < .5) # must be within .5m of db
+        if len(vals)==0:
+            mindir = np.argmin(range_data) # find direction to db
+        else:
+            for i in range(len(vals)):
+                if vals[i] > 180:
+                    vals[i] -= 360
+        mindir = np.average(vals)
+        if np.isnan(mindir):
+            mindir = 4
+        print("mindir = %s " % mindir)
+        if (np.abs(mindir) <= 2.5):
             self.turning = False
             self.twist.angular.z = 0
-            self.twist_pub.publish(Twist())
-        elif mindir < 180:
-            self.turning = True
-            self.twist.angular.z = rotspeed*(mindir)/180
         else:
             self.turning = True
-            self.twist.angular.z = rotspeed*(mindir-360)/180
+            self.twist.angular.z = rotspeed*(mindir)/180
+            if abs(self.twist.angular.z)<.01:
+                self.turning = False
+                self.twist.angular.z = 0
+        self.twist_pub.publish(self.twist)
             
     def approach_dumbbell(self,data):
-        distance = 0.25 # stay 1/2 meters away from the dumbbell
-        speed = 0.01 # m/s
+        distance = 0.22 # stay 1/4 meters away from the dumbbell
+        speed = 0.02 # m/s
         range_data = np.array(data.ranges) #convert to numpy array
         minval = min(range_data) # closest point in space
-        mindir = np.argmin(range_data) # find direction to db
         if data.ranges[0] > distance: # if we are far enough from the db
             self.twist.linear.x = speed
             self.moving = True
             self.turning = True
         else:
             self.twist.linear.x = 0
-            self.twist_pub.publish(Twist())
             self.moving = False
             self.turning = False
-
-
+        self.twist_pub.publish(self.twist)
             
     def process_scan(self,data):
         if (self.turning == True):
             self.face_dumbbell(data)
-        elif (self.moving == True):
+        if (self.moving == True):
             self.approach_dumbbell(data)
         self.twist_pub.publish(self.twist)
         
@@ -122,11 +136,12 @@ class Robot(object):
         print("resetting position...")
         self.lift_dumbbell()
         print("open gripper...")
+        self.open_gripper()
         
         print("setting orientation:")
         self.turning = True
         while (self.turning == True):
-            rate.sleep() # wait for orienting toward db
+            rospy.sleep(0.1) # wait for orienting toward db
 
         print("resetting position...")
         self.lift_dumbbell()
@@ -138,7 +153,7 @@ class Robot(object):
         print("moving toward db...")
         self.moving = True
         while (self.moving == True):
-            rate.sleep() # Wait for movement to stop
+            rospy.sleep(0.1) # Wait for movement to stop
         
         print("close gripper")
         self.close_gripper()
@@ -155,7 +170,7 @@ class Robot(object):
         self.twist_pub.publish(Twist())  # stop
         
         print("putting it back down..")
-        self.reach_dumbbell()
+        self.set_dumbbell()
         self.open_gripper()
         
         rospy.spin()
